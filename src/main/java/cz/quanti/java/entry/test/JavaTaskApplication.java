33d5c96d1c7914f459b73b04cfbd0f591c591ed1
petr122 <92915557+petr122@users.noreply.github.com>
2021-10-27 13:57:19 +0200

    Add files via upload

---
+++ src/main/java/cz/quanti/java/entry/test/JavaTaskApplication.java
@@ -0,0 +1,13 @@
+package cz.quanti.java.entry.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/quanti/java/entry/test/JavaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.quanti.java.entry.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
